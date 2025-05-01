209ff2025970075e306d54be7c690a3bde3e8871
Jerred Shepherd <jshepherd@pinterest.com>
2025-05-01 14:46:45 -0700

    few more

---
+++ type-challenges/00108-medium-trim.ts
@@ -0,0 +1,19 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Trim<'str'>, 'str'>>,
+  Expect<Equal<Trim<' str'>, 'str'>>,
+  Expect<Equal<Trim<'     str'>, 'str'>>,
+  Expect<Equal<Trim<'str   '>, 'str'>>,
+  Expect<Equal<Trim<'     str     '>, 'str'>>,
+  Expect<Equal<Trim<'   \n\t foo bar \t'>, 'foo bar'>>,
+  Expect<Equal<Trim<''>, ''>>,
+  Expect<Equal<Trim<' \n\t '>, ''>>,
+]
+
+
+// ============= Your Code Here =============
+type Trim<S extends string> = S extends `${Whitespace}${infer T extends string}` | `${infer T extends string}${Whitespace}` ? Trim<T> : S
+
+type Whitespace = " " | "\n" | "\t"
